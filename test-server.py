ac7746df0dff4fa33bc8bb7f099f26c96c9c0d41
Ian SR <iann.ipn9@gmail.com>
2021-09-12 14:37:14 -0500

    test server correcto

--- test-server.py
+++ test-server.py
@@ -2,14 +2,18 @@ import socket
 
 HOST = "192.168.1.107"
 PORT = 5432
+BUFFERSIZE = 1024
 mi_socket = socket.socket()
 mi_socket.bind((HOST, PORT))
 mi_socket.listen(5)
 
-while True:
-    conexion, addr = mi_socket.accept()
-    print ("Nueva conexion!")
-    print (addr)
+conexion, addr = mi_socket.accept()
+print ("Nueva conexion!")
+print (addr)
 
+while True:
+    peticion = conexion.recv(BUFFERSIZE).decode('UTF-8')
+    print (peticion)
     conexion.send(str.encode("Hola desde del server"))
-    conexion.close()
+
+conexion.close()

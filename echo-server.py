5334228c33a1383c0b0742f0d43239dac6022283
Ian SR <iann.ipn9@gmail.com>
2021-09-12 20:50:38 -0500

    Server funcional con flags

--- echo-server.py
+++ echo-server.py
@@ -27,7 +27,8 @@ GAMETABLE = TABLES[1]       # Inicializa un tablero
 def validPos(throwPos, playerChar):      # Verifica si la posicion es valida
     convertedPos = ""
     if throwPos[0].isdigit():
-        convertedPos = throwPos[0]
+        convertedPos = throwPos[1]
+        throwPos = str(throwPos[0]) + str(throwPos[0])
     else:
         if throwPos[0] == "A":
             convertedPos = '1'
@@ -42,10 +43,12 @@ def validPos(throwPos, playerChar):      # Verifica si la posicion es valida
         else:
             return False
     
-    if GAMETABLE[convertedPos][throwPos[1]] == '-':
-        GAMETABLE[convertedPos][throwPos[1]] = playerChar
+    if GAMETABLE[int(throwPos[1])][int(convertedPos)] == '-':
+        GAMETABLE[int(throwPos[1])][int(convertedPos)] = playerChar
+        print("pos valida "+throwPos[1]+convertedPos)
         return True
     else:
+        print("pos NO valida")
         return False
 
 def pcThrow():                          # Genera un tiro aleatorio
@@ -56,6 +59,7 @@ def pcThrow():                          # Genera un tiro aleatorio
         size = 6
     while ready == False:
         randStr = str(randrange(1,size)) + str(randrange(1,size))
+        print ("Intento de pc: "+randStr)
         ready = validPos(randStr, 'o')
 
 def tableToStr():                       # Convierte el tablero a cadena
@@ -65,17 +69,18 @@ def tableToStr():                       # Convierte el tablero a cadena
             genStr = genStr + str(GAMETABLE[i][j]) +' '
         genStr = genStr + '\n'
 
-    return str.encode(genStr)
+    return genStr
 
 def gameFinished(char2check):           # Se verifican las lineas
     size = 0
     i = 1
     j = 1
+    print("validando ganador" + str(type(char2check)))
     if GAMEDIFFICULT == 1:
         size = 4
     else:
         size = 6
-
+    print("flag1")
     while i < size:
         if GAMETABLE[i][j] == char2check:
             j+=1
@@ -83,8 +88,10 @@ def gameFinished(char2check):           # Se verifican las lineas
             i+=1
             j=1
         if j == size:
+            print("Fila")
             return True
 
+    print("flag2")
     i = 1
     while i < size:
         if GAMETABLE[j][i] == char2check:
@@ -93,96 +100,107 @@ def gameFinished(char2check):           # Se verifican las lineas
             i+=1
             j=1
         if j == size:
+            print("Columna")
             return True
 
     i = 1
     j = 1
+    print("flag3")
     while i < size:
         if GAMETABLE[i][j] == char2check:
             i+=1
             j+=1
-    if i == size:
+        else:
+            i = size
+    if j == size:
+        print("Diagonal 1")
         return True
     
     i = 1
-    j = 3
+    j = size-1
+    print("flag4")
     while i < size:
         if GAMETABLE[i][j] == char2check:
             i+=1
             j-=1
-    if i == size:
+        else:
+            i = size
+    if j == 0:
+        print("diagonal 2")
         return True
-    else:
-        return False
-
-def main():
-    CONN, ADDR = MYSOCKET.accept()      # En espera de cliente
-    print ("Nueva conexion!")
-    print (ADDR)
-
-    STATESTR = ""                       # Se inicializan valores para el juego
-    WINNERSTR = ""
-    AGAINSTR = ""
-    THROWCOUNTER = 0
     
-    while (GAMESTATE == 0):             # Bienvenida al gato dummy
+    print("Aun no gana nadie")
+    return False
 
-        STATESTR = "Escribe la cordenada de tu siguiente tiro"
-        AGAINSTR = "Juegas de nuevo?\n(1)Si\n(2)No"
-        THROWCOUNTER = 0
-        GAMESTATE = 1
 
-        while GAMESTATE == 1:       # Preguntando por dificultad
-            CONN.send(str.encode("Bienvenido al Gato Dummy :)\n"
-                            +"Elige dificultad\n(1) Normal\n(2) Avanzado"))
-            gd_data = int(CONN.recv(BUFFERSIZE).decode('UTF-8'))
-            print("Dificultad elegida: "+ str(gd_data))
+print("En espera ... ")
+CONN, ADDR = MYSOCKET.accept()      # En espera de cliente
+print ("Nueva conexion!")
+print (ADDR)
 
-            if gd_data == 1:
-                GAMEDIFFICULT = 1
-                GAMESTATE = 2
-                THROWCOUNTER = 5
-                GAMETABLE = TABLES[0]
-            elif gd_data == 2:
-                GAMEDIFFICULT = 2
-                GAMESTATE = 2
-                THROWCOUNTER = 13
-                GAMETABLE = TABLES[1]
-            else:
-                gd_data = ""
-        
-        while GAMESTATE == 2:       # Desarrollo del juego
-            if THROWCOUNTER > 0:
-                CONN.send(str.encode(STATESTR))
-                gt_data = CONN.recv(BUFFERSIZE).decode('UTF-8')
+STATESTR = ""                       # Se inicializan valores para el juego
+WINNERSTR = ""
+AGAINSTR = ""
+THROWCOUNTER = 0
 
-                if validPos(gt_data, 'x'):
-                    STATESTR = "Escribe la cordenada de tu siguiente tiro"
-                    if gameFinished('x'):
-                        WINNERSTR = "Has ganado!\n"
-                        GAMESTATE = 3
-                    else:
-                        THROWCOUNTER -= 1
-                        pcThrow()
-                        if gameFinished('o'):
-                            WINNERSTR = "Te han ganado!\n"
-                            GAMESTATE = 3
+while (GAMESTATE == 0):             # Bienvenida al gato dummy
+
+    STATESTR = "Escribe la cordenada de tu siguiente tiro"
+    AGAINSTR = "Juegas de nuevo?\n(1)Si\n(2)No"
+    THROWCOUNTER = 0
+    GAMESTATE = 1
+
+    while GAMESTATE == 1:       # Preguntando por dificultad
+        CONN.send(str.encode("Bienvenido al Gato Dummy :)\n"
+                        +"Elige dificultad\n(1) Normal\n(2) Avanzado"))
+        gd_data = int(CONN.recv(BUFFERSIZE).decode('UTF-8'))
+        print("Dificultad elegida: "+ str(gd_data))
+
+        if gd_data == 1:
+            GAMEDIFFICULT = 1
+            GAMESTATE = 2
+            THROWCOUNTER = 5
+            GAMETABLE = TABLES[0]
+        elif gd_data == 2:
+            GAMEDIFFICULT = 2
+            GAMESTATE = 2
+            THROWCOUNTER = 13
+            GAMETABLE = TABLES[1]
+        else:
+            gd_data = ""
+    
+    while GAMESTATE == 2:       # Desarrollo del juego
+        if THROWCOUNTER > 0:
+            CONN.send(str.encode(tableToStr() +STATESTR))
+            gt_data = CONN.recv(BUFFERSIZE).decode('UTF-8')
+            print ("Tiro elegido: " + str(gt_data))
+
+            if validPos(gt_data, 'x'):
+                STATESTR = "Escribe la cordenada de tu siguiente tiro"
+                if gameFinished('x'):
+                    WINNERSTR = tableToStr() + "Has ganado!\n"
+                    GAMESTATE = 3
                 else:
-                    STATESTR = "No puedes tirar ahi\nElige un nuevo lugar"
+                    THROWCOUNTER -= 1
+                    pcThrow()
+                    if gameFinished('o'):
+                        WINNERSTR =  tableToStr() + "Te han ganado!\n"
+                        GAMESTATE = 3
             else:
-                WINNERSTR = "Empate! Ya no quedan mas tiros\n"
-                GAMESTATE = 3
+                STATESTR = "No puedes tirar ahi\nElige un nuevo lugar"
+        else:
+            WINNERSTR = "Empate! Ya no quedan mas tiros\n"
+            GAMESTATE = 3
 
-        while GAMESTATE == 3:       # Quieres jugar de nuevo?
-            AGAINSTR = WINNERSTR + AGAINSTR
-            CONN.send(str.encode(AGAINSTR))
-            ng_data = int(CONN.recv(BUFFERSIZE).decode('UTF-8'))
+    while GAMESTATE == 3:       # Quieres jugar de nuevo?
+        AGAINSTR = WINNERSTR + AGAINSTR
+        CONN.send(str.encode(AGAINSTR))
+        ng_data = int(CONN.recv(BUFFERSIZE).decode('UTF-8'))
 
-            if ng_data == 1:
-                GAMESTATE = 1
-            elif ng_data == 2:
-                GAMESTATE == 0
-    CONN.close();
-
-if __name__ == '__main__':
-    main()
+        if ng_data == 1:
+            print("va de nuez")
+            GAMESTATE = 0
+        elif ng_data == 2:
+            print("Bye")
+            GAMESTATE = 4
+CONN.close();
